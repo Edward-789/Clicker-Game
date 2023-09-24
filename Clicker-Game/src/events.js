0b87f545c025a09491a6b7b8587ae9835892da97
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-09-24 15:37:31 +0100

    change increment stuff

--- Clicker-Game/src/events.js
+++ Clicker-Game/src/events.js
@@ -5,4 +5,4 @@ const cookieNum = document.querySelector("#numCookies")
 let mainButtons = new mainButton(1)
 
 clickerButton.addEventListener("click", function() {mainButtons.upClicks});
-minerButton.addEventListener("click", function() {let i = new autoUpgrade(1, 5000, document.querySelector("#costMiners"), document.querySelector("#amountMiners"))})
+minerButton.addEventListener("click", function() {let i = new autoUpgrade(1, 5000, document.querySelector("#costMiners"), document.querySelector("#amountMiners"), 0)})
