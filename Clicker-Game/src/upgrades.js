876d19a9dc14d7ade10c70d0c0f573e5ab5a0b8e
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-09-24 23:50:24 +0100

    refactor

--- Clicker-Game/src/upgrades.js
+++ Clicker-Game/src/upgrades.js
@@ -1,33 +1,28 @@
 let increments = [10, 12, 25];
+let pointsPerSec = 0;
 
-class mainButton {
-    constructor(increment) {
-        this.increment = increment;
-    }
-
-    get upClicks() {
-        cookieNum.innerText = (cookieNum.innerText * 1) + this.increment;
-    }
+function upClicks() {
+        cookieNum.innerText = (cookieNum.innerText * 1) + 1;
 }
 
-class autoUpgrade {
-    constructor(increment, interval, costObject, amountObject, typeOfUpgrade) {
-        if (costObject.innerText * 1 < cookieNum.innerText * 1) {
-            amountObject.innerText = (amountObject.innerText * 1) + 1;
+function autoUpgrade(increment, interval, costObject, amountObject, typeOfUpgrade) {
+    if (costObject.innerText * 1 < cookieNum.innerText * 1) {
+        amountObject.innerText = (amountObject.innerText * 1) + 1;
 
-            cookieNum.innerText = (cookieNum.innerText * 1) - (costObject.innerText * 1);
+        cookieNum.innerText = (cookieNum.innerText * 1) - (costObject.innerText * 1);
 
-            costObject.innerText = (costObject.innerText * 1) + increments[typeOfUpgrade];
+        costObject.innerText = (costObject.innerText * 1) + increments[typeOfUpgrade];
 
-            if (amountObject.innerText % 8 == 0) {
-                increments[typeOfUpgrade] = Math.round(increments[typeOfUpgrade] * 1.1)
-            }
+        pointsPerSec += increment * (1000 / interval)
 
-            setInterval(function() {
-                cookieNum.innerText = (cookieNum.innerText * 1) + increment;
-            }, interval)
-        } else {
-            alert("youre too poor")
+        if (amountObject.innerText % 8 == 0) {
+            increments[typeOfUpgrade] = Math.round(increments[typeOfUpgrade] * 1.1)
         }
+
+        setInterval(function() {
+            cookieNum.innerText = (cookieNum.innerText * 1) + increment;
+        }, interval)
+    } else {
+        alert("youre too poor")
     }
 }
