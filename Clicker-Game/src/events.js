cb37636e24d249a07346acfae69230aa037414fb
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-09-24 20:37:32 +0100

    add saving

--- Clicker-Game/src/events.js
+++ Clicker-Game/src/events.js
@@ -2,11 +2,30 @@ const clickerButton = document.querySelector("#mainButton");
 const minerButton = document.querySelector("#minerButton");
 const treeButton = document.querySelector("#treeButton");
 const sauropodButton = document.querySelector("#sauropodButton");
-const cookieNum = document.querySelector("#numCookies")
+const cookieNum = document.querySelector("#numCookies");
 
-let mainButtons = new mainButton(1)
+const costMiners = document.querySelector("#costMiners");
+const amountMiners = document.querySelector("#amountMiners");
+const costTrees = document.querySelector("#costTrees");
+const amountTrees = document.querySelector("#amountTrees");
+const costSauropods = document.querySelector("#costSauropods");
+const amountSauropods = document.querySelector("#amountSauropods");
 
-clickerButton.addEventListener("click", function() {mainButtons.upClicks});
-minerButton.addEventListener("click", function() {let i = new autoUpgrade(1, 5000, document.querySelector("#costMiners"), document.querySelector("#amountMiners"), 0)})
-treeButton.addEventListener("click", function() {let i = new autoUpgrade(1, 2000, document.querySelector("#costTrees"), document.querySelector("#amountTrees"), 1)})
-sauropodButton.addEventListener("click", function() {let i = new autoUpgrade(1, 333, document.querySelector("#costSauropods"), document.querySelector("#amountSauropods"), 2)})
+let mainButtons = new mainButton(1);
+
+clickerButton.addEventListener("click", function() {
+  mainButtons.upClicks;
+  console.log(findSave());
+});
+
+minerButton.addEventListener("click", function() {
+  let i = new autoUpgrade(1, 5000, costMiners, amountMiners, 0);
+});
+
+treeButton.addEventListener("click", function() {
+  let i = new autoUpgrade(1, 2000, costTrees, amountTrees, 1);
+});
+
+sauropodButton.addEventListener("click", function() {
+  let i = new autoUpgrade(1, 333, costSauropods, amountSauropods, 2);
+});
