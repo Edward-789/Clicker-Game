9fea9574772c0576fa0156d57141b19e420a98aa
Edward-789 <142745633+Edward-789@users.noreply.github.com>
2023-09-24 16:54:11 +0100

    add a bunch of upgrades

--- Clicker-Game/src/events.js
+++ Clicker-Game/src/events.js
@@ -1,8 +1,12 @@
 const clickerButton = document.querySelector("#mainButton");
 const minerButton = document.querySelector("#minerButton");
+const treeButton = document.querySelector("#treeButton");
+const sauropodButton = document.querySelector("#sauropodButton");
 const cookieNum = document.querySelector("#numCookies")
 
 let mainButtons = new mainButton(1)
 
 clickerButton.addEventListener("click", function() {mainButtons.upClicks});
 minerButton.addEventListener("click", function() {let i = new autoUpgrade(1, 5000, document.querySelector("#costMiners"), document.querySelector("#amountMiners"), 0)})
+treeButton.addEventListener("click", function() {let i = new autoUpgrade(1, 2000, document.querySelector("#costTrees"), document.querySelector("#amountTrees"), 1)})
+sauropodButton.addEventListener("click", function() {let i = new autoUpgrade(1, 333, document.querySelector("#costSauropods"), document.querySelector("#amountSauropods"), 2)})
